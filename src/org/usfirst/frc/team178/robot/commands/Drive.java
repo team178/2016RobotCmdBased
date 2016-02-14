40783796bd6bd5176eeba793f169729b42b46c0e
poojpooj <pswami1600@gmail.com>
2016-02-14 14:13:17 -0500

    Finished DriveTrain and Drive code close #3 and close #4

--- src/org/usfirst/frc/team178/robot/commands/Drive.java
+++ src/org/usfirst/frc/team178/robot/commands/Drive.java
@@ -1,47 +1,58 @@
 package org.usfirst.frc.team178.robot.commands;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team178.robot.Robot;
 import org.usfirst.frc.team178.robot.subsystems.DriveTrain;
 
-public class Drive {
+public class Drive extends Command{
+	DriveTrain drivetrain;
+	Joystick joystick;
+	double yVal,twistVal;
 	
 	public Drive()
 	{
 		requires(Robot.drivetrain);
+		requires(Robot.oi.joystick);
+
+		drivetrain = Robot.drivetrain;
+		joystick = Robot.oi.joystick; 
 	}
 	
-	public void drive(Joystick joystick){
+	protected void initialize() {
+	}
+	
+	protected void execute() {
 		//Joystick returns from -1 to 1, motor takes values from -1 to 1.
 		//TODO clean up this section. The negatives are quite ghetto. It's hard to understand.
-		double yVal,twistVal;
 		yVal = -1*joystick.getY();
 		twistVal = -1*joystick.getTwist();
-				
+
 		// 6wl tank drive has two motors on one gearbox that drive in the same direction.
 		//TODO Debug and optimize this code. It does things weirdly. It's more logical to turn based on twist.
 		//The if condition implements what's called a dead zone. The controllers have some variances to them, 
 		//and this makes sure that the robot doesn't do anything we don't want it to.
-		if(Math.abs(yVal)>0.1 || Math.abs(twistVal)>0.1){
-			left1.set(-twistVal+yVal);
-			left2.set(-twistVal+yVal);
-			right1.set(-twistVal-yVal);
-			right2.set(-twistVal-yVal);
-		}
 		//Without this, the motor speed is never upset. 
 		//The robot would continue moving at its last speed. This makes it stop.
-		else{
-			left1.set(0);
-			left2.set(0);
-			right1.set(0);
-			right2.set(0);
+
+		drivetrain.drive(-twistVal+yVal, -twistVal-yVal);
+
+	}
+
+	protected boolean isFinished() {
+		if(Math.abs(yVal)>0.1 || Math.abs(twistVal)>0.1){
+			return false;
 		}
-		
-		//SmartDashboard.putNumber("Rotation", );
+		else {
+			return true;
+		}
 	}
-	
-public void initDefaultCommand() {
-		
+
+	protected void end() {
+		drivetrain.drive(0,0);
 	}
-	
+
+	protected void interrupted() {
+		drivetrain.drive(0,0);
+	}
 }
--- src/org/usfirst/frc/team178/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team178/robot/subsystems/DriveTrain.java
@@ -2,41 +2,34 @@ package org.usfirst.frc.team178.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Ultrasonic;
-import edu.wpi.first.wpilibj.TalonSRX;
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class DriveTrain extends Subsystem
 {
 
-	public static TalonSRX left1;
-	public static TalonSRX left2;
-	public static TalonSRX right1;
-	public static TalonSRX right2;
-
+	public static CANTalon left1;
+	public static CANTalon left2;
+	public static CANTalon right1;
+	public static CANTalon right2;
 	
 	public DriveTrain()  {
 		
-		left1 = new TalonSRX(0);
-		left2 = new TalonSRX(1);
-		right1 = new TalonSRX(2);
-		right2 = new TalonSRX(3);
+		left1 = new CANTalon(0);
+		left2 = new CANTalon(1);
+		right1 = new CANTalon(2);
+		right2 = new CANTalon(3);
 	}
 	protected void initDefaultCommand() {
 		
 	}
 	
-	protected void execute()
-	{
-		
-	}
+	public void drive(double leftnum, double rightnum){
+    	left1.set(leftnum);
+    	left2.set(leftnum);
+    	right1.set(rightnum);
+    	right2.set(rightnum);
+    }
 	
-	protected boolean isFinished()
-	{
-		return false;
-	}
-	
-	protected void end() {}
-	
-	protected void interrupted() {}
 
 }
