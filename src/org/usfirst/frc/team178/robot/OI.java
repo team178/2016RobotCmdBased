fb3923ab1ef820ee534c3e0203975c7f2ecd9bd3
Giselle <gisellegk@users.noreply.github.com>
2016-02-14 19:08:59 -0500

    Remove some git merge stuff

--- src/org/usfirst/frc/team178/robot/OI.java
+++ src/org/usfirst/frc/team178/robot/OI.java
@@ -2,12 +2,8 @@ package org.usfirst.frc.team178.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
-<<<<<<< HEAD
-=======
 import org.usfirst.frc.team178.robot.commands.*;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
->>>>>>> 51f432dd03f4f093d8a0b1be0a517346e06e801a
-
 
 /**
  * This class is the glue that binds the controls on the physical operator
