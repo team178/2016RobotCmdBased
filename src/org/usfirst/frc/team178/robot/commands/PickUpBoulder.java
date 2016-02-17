20c1d82b0db53657f920fa48374ab45d3b74f25d
SilverSapo <zergling13rush@gmail.com>
2016-02-17 17:20:16 -0500

    Attempted to allign with Pooja's code

--- src/org/usfirst/frc/team178/robot/commands/PickUpBoulder.java
+++ src/org/usfirst/frc/team178/robot/commands/PickUpBoulder.java
@@ -29,7 +29,7 @@ public class PickUpBoulder extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	if(Robot.sensor.boulderIn()){
+    	if(Robot.sensor.getstuff()){
     		return true;
     	}
     	else{
