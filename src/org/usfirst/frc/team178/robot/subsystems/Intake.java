12d52133e231ffcbe9dd8f2f91d7565ce8f749cc
Matt <TheKrunch@users.noreply.github.com>
2016-02-18 17:52:56 -0500

    Created more singular methods for commands.

--- src/org/usfirst/frc/team178/robot/subsystems/Intake.java
+++ src/org/usfirst/frc/team178/robot/subsystems/Intake.java
@@ -14,6 +14,36 @@ public class Intake extends Subsystem {
 		inOut = new CANTalon(18);
 		upDown = new CANTalon(19);
 	}
+	
+	public void in()//Intakes ball.
+	{
+		inOut.set(-1);
+	}
+	
+	public void out()//Outakes ball.
+	{
+		inOut.set(1);
+	}
+	
+	public void inOutStop()//Stops pulleys/wheels.
+	{
+		inOut.set(0);
+	}
+	
+	public void up()//Raises intake.
+	{
+		upDown.set(1);
+	}
+	
+	public void down()//Lowers intake.
+	{
+		upDown.set(-1);
+	}
+	
+	public void upDownStop()//Stops all up down.
+	{
+		upDown.set(0);
+	}
     
 	public void lift()
 	{
@@ -21,7 +51,8 @@ public class Intake extends Subsystem {
 		upDown.set(0.25);
 	}
 	
-	public void stop()
+	
+	public void stop()//Stops all intake motors.
 	{
 		inOut.set(0);
 		upDown.set(0);
