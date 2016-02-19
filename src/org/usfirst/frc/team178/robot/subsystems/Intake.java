3676d9967938c78dad46c220708e43e96886cfda
TheKrunch <mattcrunch@gmail.com>
2016-02-18 19:21:18 -0500

    Stream lined intake subsystem methods

--- src/org/usfirst/frc/team178/robot/subsystems/Intake.java
+++ src/org/usfirst/frc/team178/robot/subsystems/Intake.java
@@ -15,54 +15,23 @@ public class Intake extends Subsystem {
 		upDown = new CANTalon(19);
 	}
 	
-	public void in()//Intakes ball.
+	public void setInOut(int value)
 	{
-		inOut.set(-1);
+		inOut.set(value);
 	}
 	
-	public void out()//Outakes ball.
+	public void setUpDown(int value)
 	{
-		inOut.set(1);
-	}
-	
-	public void inOutStop()//Stops pulleys/wheels.
-	{
-		inOut.set(0);
-	}
-	
-	public void up()//Raises intake.
-	{
-		upDown.set(1);
-	}
-	
-	public void down()//Lowers intake.
-	{
-		upDown.set(-1);
-	}
-	
-	public void upDownStop()//Stops all up down.
-	{
-		upDown.set(0);
-	}
-    
-	public void lift//This spins a ball out of the robot and at the same time raises the robot at .25 power.
-	{
-		inOut.set(1);
-		upDown.set(0.25);
+		upDown.set(value);
 	}
 	
 	
-	public void stop()//Stops all intake motors.
+	public void allStop()//Stops all intake motors.
 	{
 		inOut.set(0);
 		upDown.set(0);
 	}
 	
-	public void reverse()//This spins a ball into the robot and leaves the intake where it is.
-	{
-		inOut.set(-1);
-		upDown.set(0);
-	}
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
