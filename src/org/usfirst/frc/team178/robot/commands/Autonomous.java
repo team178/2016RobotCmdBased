2b5bfd33714f0c74fb29cc463c341cbc277758e4
Nandan Tumu <t.nandan2008@gmail.com>
2016-02-23 17:00:45 -0500

    Committed new VisionVars class that forces usage of methods.

--- src/org/usfirst/frc/team178/robot/commands/Autonomous.java
+++ src/org/usfirst/frc/team178/robot/commands/Autonomous.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class Autonomous extends CommandGroup {
     
     public  Autonomous() {
-    	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    	
         // Add Commands here:
         // e.g. addSequential(new Command1());
         //      addSequential(new Command2());
