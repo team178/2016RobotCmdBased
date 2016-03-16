43dc3657adfe1afe70ac7709b4a21c7738546631
Team 178 <team178@farmingtonrobotics.org>
2016-03-16 19:31:00 -0400

    Pre Dartmouth code part 2

---
+++ src/org/usfirst/frc/team178/robot/commands/DropIntake.java
@@ -0,0 +1,49 @@
+package org.usfirst.frc.team178.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import org.usfirst.frc.team178.robot.Robot;
+import org.usfirst.frc.team178.robot.subsystems.*;
+
+/**
+ *
+ */
+public class DropIntake extends Command {
+	
+	Intake intake;
+
+    public DropIntake() {
+    	requires(Robot.intake);
+    	this.setTimeout(0.5);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	intake = Robot.intake;
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.intake.setUpDown(0.5);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	return (this.isTimedOut());
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.intake.setUpDown(0);
+		Robot.intake.allStop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	Robot.intake.setUpDown(0);
+		Robot.intake.allStop();
+    }
+}
---
+++ src/org/usfirst/frc/team178/robot/commands/SpinIntakeIn.java
@@ -0,0 +1,50 @@
+package org.usfirst.frc.team178.robot.commands;
+
+import org.usfirst.frc.team178.robot.Robot;
+import org.usfirst.frc.team178.robot.subsystems.Intake;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class SpinIntakeIn extends Command {
+	
+
+    public SpinIntakeIn() {
+        requires(Robot.intake);
+        //this.setTimeout(0.5);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.intake.setInOut(-1);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	
+    	return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.intake.setInOut(0);
+		Robot.intake.allStop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    /* So basically we had to do this because we wanted this to go to two buttons
+    but you can't do this so basically two buttons will call the same command and it will stop
+    from being interrupted after the timeout*/
+    
+    protected void interrupted() {
+    	Robot.intake.setInOut(0);
+		Robot.intake.allStop();
+    }
+}
